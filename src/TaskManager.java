aafba647bf9080f56d6c9d1fc82e3f9dff588e70
Bartosz Tobiasz <barticz90@gmail.com>
2021-12-09 19:21:00 +0100

    Add an app loop and create its core

--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class TaskManager {
     public static void main(String[] args) {
+        Scanner terminalReader = new Scanner(System.in);
+        String command = "";
+        String[][] tasks = new String[0][0];
 
+        while (!command.equals("exit")) {
+            switch (command) {
+                case "add":
+                    // add
+                    break;
+                case "remove":
+                    // remove
+                    break;
+                case "list":
+                    // list
+                    break;
+            }
+
+            System.out.println(ConsoleColors.BLUE + "Please select an option:");
+            System.out.println(ConsoleColors.RESET + "add\nremove\nlist\nexit");
+            command = terminalReader.nextLine().trim();
+        }
+        System.out.println("Bye, bye.");
     }
 }
