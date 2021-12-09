19a191e93a4d801cf94186b44489cfec006c3d52
Bartosz Tobiasz <barticz90@gmail.com>
2021-12-09 20:51:07 +0100

    Add Update Tasks From File feature

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,10 +1,16 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class TaskManager {
     public static void main(String[] args) {
         Scanner terminalReader = new Scanner(System.in);
         String command = "";
-        String[][] tasks = new String[0][0];
+        Path CSVFilePath = Paths.get("src", "main", "java", "tasks.csv");
+        String[][] tasks = updateTasksFromFile(CSVFilePath);
 
         while (!command.equals("exit")) {
             switch (command) {
@@ -24,5 +30,21 @@ public class TaskManager {
             command = terminalReader.nextLine().trim();
         }
         System.out.println("Bye, bye.");
+    }
+
+    public static String[][] updateTasksFromFile(Path tasksFilePath) {
+        try {
+            List<String> lines = Files.readAllLines(tasksFilePath);
+            String[][] updatedTasks = new String[lines.size()][0];
+            int i = 0;
+            for (String line : lines) {
+                updatedTasks[i] = line.split(", ");
+                i++;
+            }
+            return updatedTasks;
+        } catch (IOException e) {
+            System.out.println("incorrect file path");
+            return new String[0][0];
+        }
     }
 }
